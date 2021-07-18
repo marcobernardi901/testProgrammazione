e2b7af1f4f17871115f6057ce4ca63fdf740ee80
marcobernardi901 <87600002+marcobernardi901@users.noreply.github.com>
2021-07-18 11:22:23 +0200

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,5 +1,6 @@
 public class Test{
   public static void main(String[]args){
     System.out.println("Sono nel main di test");
+    system.out.println("Visualizzazione riga nuova da seconda utenza");
   }
 } 
